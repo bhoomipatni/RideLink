afd74593d70c338ec4717b3fe3bd67429f2667c6
0ce10t <96030922+0ce10tsgit@users.noreply.github.com>
2026-02-03 17:31:01 -0500

    added sqlalchemy models
    Added the intial models for SQL Alchemy based on the current schema

--- backend/models.py
+++ backend/models.py
@@ -15,11 +15,24 @@ Base() = declarative_base()
 
 # example model
 class User(Base):
-    __tablename__ = 'users'
-
-    id = Column(Integer, primary_key=True)
+    __tablename__ = 'Users'
+    id = Column(Integer, primary_key=True) # postgreSQL uses serial so no need for autoincrement
     username = Column(String(50), unique=True, nullable=False)
     email = Column(String(100), unique=True, nullable=False)
+    password = Column(String(100), nullable=False)
+    rcsid = Column(String(20), unique=True, nullable=False)
+
+class Rides(Base):
+    __tablename__ = 'Rides'
+    id = Column(Integer, primary_key=True,)  # postgreSQL uses serial so no need for autoincrement
+    driverid = Column(Integer, nullable=False)
+    address = Column(String(200), nullable=False)
+    cost = Column(Float, nullable=False, default=0.0)
+    isactive = Column(Boolean, default=True, nullable=False)
+    description = Column(String(500), nullable=True)
+    date = Column(DateTime, default=datetime.timezone.now, nullable=False)
+
+    
 
 # create all tables
 Base.metadata.create_all(engine)
