16e1fad37555482f37903553fb9bb4606e309689
aniket132123 <singani17@outlook.com>
2026-02-10 16:27:19 -0500

    updated models and main

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy import Boolean, Float, create_engine, Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import datetime
@@ -37,26 +37,4 @@ class Rides(Base):
 # create all tables
 Base.metadata.create_all(engine)
 
-# insert the data into database and start a session
-Session = sessionmaker(bind=engine)
-session = Session()
 
-# insert a user to the table during the session
-# ALWAYS COMMIT WHEN YOU ARE ADDING OR EDITING DATA IN THE DATABASE OR CHANGES WILL NOT BE SAVED
-new_user = User(username='Sandy', email='sandy@gmail.com', password='cool-password')
-session.add(new_user)
-session.commit()
-
-# THESE ARE PURELY EXAMPLES OF HOW TO QUERY THE DATABASE WE WILL PROBABLY BE DOING QUERIES IN main.py OR ANOTHER FILE
-# AND LEAVE THIS FILE PURELY FOR THE MODELS AND DATABASE CONNECTION
-# Example: query some data from the database and print
-all_users = session.query(User).all()
-for user in all_users:
-    print(user.username, user.email)
-
-# Example: Querying a specific user by their username
-user = session.query(User).filter_by(username='Sandy').first()
-print(user.username)
-
-# close the session
-session.close()
