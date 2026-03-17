5d13334d815b25038f8691ad237aa7c4c85ccea4
Sese314 <166954447+Sese314@users.noreply.github.com>
2026-03-17 18:58:04 -0400

    Testing Listing Rides
    - Testing the my trips pages to see if it can open new tabs based on a user's id or trip's id
    - Generated pages are empty right now and no updated css yet but new pages are working?
    - test.json is also a small test file
    - a few css things are funky in the other tabs too because I'm just testing features out right now, don't actually use this stuff yet

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,36 @@
-//testing "database"
-const rides = [
-  { driver: "Alex", destination: "Boston", time: "5pm" },
-  { driver: "Sam", destination: "NYC", time: "3pm" }
-];
+//used for trips_details.html -> load in info for an individual trip
+async function loadIndTrip() {
+    //get the id from the URL
+    const id = new URLSearchParams(location.search).get('id');
+
+    //fetch specific trip from your backend
+    const response = await fetch(`/api/trips/${id}`);
+    const trip = await response.json();
+
+    //get relevant data
+    document.getElementById('destination').textContent = trip.destination;
+    document.getElementById('dateNtime').textContent = `${trip.dayname} ${trip.date} @ ${trip.time}`;
+    document.getElementById('driver').textContent = `with ${trip.driver}`;
+    document.getElementById('distance').textContent = `${trip.distance} miles`;
+    document.getElementById('cost').textContent = `$${trip.cost}`;
+    document.getElementById('payment').textContent = trip.payment_method;
+    document.getElementById('riders').textContent = `${trip.num_riders} other riders`;
+}
+loadIndTrip.apply();
+
+//used for trips.html -> loads in the list
+async function loadTrips() {
+  const response = await fetch('test.json'); //this is just to see how the list looks with fake user input
+  const trips = await response.json();
+
+  trips.forEach(trip => {
+    document.getElementById('trip-list').innerHTML += `
+      <a href="trips_details.html?id=${trip.id}" class="trip-card">
+        <span>${trip.destination}</span>
+        <span>${trip.date}</span>
+        <span>$${trip.cost}</span>
+      </a>
+    `;
+  });
+}
+loadTrips();
