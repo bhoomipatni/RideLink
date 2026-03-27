57ee39b1ecd316f09a8a2ccc47184e7421c9ee1f
Sese314 <166954447+Sese314@users.noreply.github.com>
2026-03-27 17:45:31 -0400

    Most updated for JS & CSS
    - Updated CSS on the My Trips list page
    - Updated JS for the tab button/active button stuff to show either upcoming or past trips

--- frontend/script.js
+++ frontend/script.js
@@ -3,7 +3,7 @@ async function loadIndTrip() {
     //get the id from the URL
     const id = new URLSearchParams(location.search).get('id');
 
-    //DON'T DELETE
+    //DON'T DELETE yet
     //fetch specific trip from your backend
     //const response = await fetch(`/api/trips/${id}`);
     //const trip = await response.json();
@@ -24,15 +24,32 @@ async function loadIndTrip() {
 }
 loadIndTrip.apply();
 
+
 //used for trips.html -> loads in the list
-async function loadTrips() {
+async function loadTrips(filter = 'upcoming') {
   const response = await fetch('test.json'); //this is just to see how the list looks with fake user input
   const trips = await response.json();
 
+  const today = new Date();
+  const upcomingList = []; //temp array holders whenever new user loads the page
+  const pastList = [];
+
   trips.forEach(trip => {
+    const tripDate = new Date(trip.date);
+    if (tripDate >= today) {
+      upcomingList.push(trip);
+    } else {
+      pastList.push(trip);
+    }
+  });
+
+  const toShow = filter === 'upcoming' ? upcomingList : pastList;
+
+  document.getElementById('trip-list').innerHTML = '';
+  toShow.forEach(trip => {
     document.getElementById('trip-list').innerHTML += `
       <a href="trips_details.html?id=${trip.id}" class="trip-card">
-        <h3>${trip.destination}</h3>
+        <h4>${trip.destination}</h4>
         <p>${trip.date}</p>
         <p>$${trip.cost}</p>
       </a>
@@ -40,3 +57,9 @@ async function loadTrips() {
   });
 }
 loadTrips();
+
+function showTab(tab) { //used for switching between Upcoming & Past trips in My Trips page
+  loadTrips(tab);
+  document.querySelectorAll('.tab').forEach(btn => btn.classList.remove('active'));
+  event.target.classList.add('active');
+}
