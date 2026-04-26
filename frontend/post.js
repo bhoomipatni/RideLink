9b3c65311f39239ca01af6fa12b2f6a2eff3835b
margie cao <margiecao331@gmail.com>
2026-04-25 22:52:31 -0400

    edits

--- frontend/post.js
+++ frontend/post.js
@@ -25,3 +25,8 @@ submit.addEventListener("click", async () => {
         console.error("fetch error:", e);
     }
 });
+
+document.getElementById("seats").addEventListener("input", function() {
+  if (this.value > 25) this.value = 25;
+  if (this.value < 1 && this.value !== "") this.value = 1;
+});
